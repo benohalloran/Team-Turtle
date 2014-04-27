1834808a87c78d2ccd0438fcc320708e4074d75f
Ben O'Halloran <bohalloran95@gmail.com>
2014-04-26 22:03:14 -0400

    Lighter Background
    Lighter background image. Makes it more readable.

--- src/com/pseudosudostudios/teamturtle/UniqueLinkedList.java
+++ src/com/pseudosudostudios/teamturtle/UniqueLinkedList.java
@@ -17,7 +17,7 @@ public class UniqueLinkedList extends LinkedList<Task> {
 			super.add(location, object);
 		else {
 			Task t = super.remove(indexOf(object));
-			add(0, t);
+			add(0, t); // This is why I'm using a linked List
 		}
 	}
 
